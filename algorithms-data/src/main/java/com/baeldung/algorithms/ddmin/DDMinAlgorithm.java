e46ddcea79b43e5c5f45c798bf358a6c78fa8613
microcosmx <15110240026@fudan.edu.cn>
2018-04-08 15:19:57 +0800

    ref

--- algorithms-data/src/main/java/com/baeldung/algorithms/ddmin/DDMinAlgorithm.java
+++ algorithms-data/src/main/java/com/baeldung/algorithms/ddmin/DDMinAlgorithm.java
@@ -8,22 +8,6 @@ import java.util.stream.Collectors;
 import org.apache.commons.collections4.CollectionUtils;
 
 public class DDMinAlgorithm {
-
-	public int runBinarySearchRecursively(int[] sortedArray, int key, int low, int high) {
-
-		int middle = (low + high) / 2;
-		if (high < low) {
-			return -1;
-		}
-
-		if (key == sortedArray[middle]) {
-			return middle;
-		} else if (key < sortedArray[middle]) {
-			return runBinarySearchRecursively(sortedArray, key, low, middle - 1);
-		} else {
-			return runBinarySearchRecursively(sortedArray, key, middle + 1, high);
-		}
-	}
 	
 	
 	private DDMinDelta ddmin_delta = null;
