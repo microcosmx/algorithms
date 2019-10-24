22e6b6f4f26df7401f361fcb39d3f883b9e3eff8
xgzx123 <xgzx123xgzx123@163.com>
2019-10-24 16:59:20 +0800

    ref

---
+++ algorithm-python/algorithm/test_sort.py
@@ -0,0 +1,14 @@
+
+
+from algorithms.sort import (
+    bucket_sort,
+    next_sort,
+    quick_sort
+)
+
+
+print(bucket_sort([1,2,3,7,8,9,4,5,6,1,1,2]))
+print(quick_sort([1,2,3,7,8,9,4,5,6,1,1,2]))
+
+
+
